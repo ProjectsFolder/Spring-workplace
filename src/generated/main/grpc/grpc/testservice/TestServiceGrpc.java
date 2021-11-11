6fa96fa0ca74d7adff360efff264bb7f868af10b
asylum29 <ksenon3@mail.ru>
2021-11-11 14:53:52 +0700

    fix

--- src/generated/main/grpc/grpc/testservice/TestServiceGrpc.java
+++ src/generated/main/grpc/grpc/testservice/TestServiceGrpc.java
@@ -6,7 +6,7 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
  */
 @javax.annotation.Generated(
     value = "by gRPC proto compiler (version 1.42.0)",
-    comments = "Source: TestService.proto")
+    comments = "Source: testservice.proto")
 @io.grpc.stub.annotations.GrpcGenerated
 public final class TestServiceGrpc {
 
